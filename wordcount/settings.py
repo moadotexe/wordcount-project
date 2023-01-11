23319098bdc8276feb0b1bc3f0b94c161f5438e5
moadotexe <jbanzon@ucsc.edu>
2023-01-10 22:24:43 -0800

    DUDE

--- wordcount/settings.py
+++ wordcount/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k5is_v@d*_jn0o$-w&y_3svjovpehs)dp-v5bqm0j&jy+dvl$x'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
